47b6916015be5004a3d975c375b36c814cdf89ac
j-lucea <jonathan.lucea@gmail.com>
2023-10-18 07:37:38 +0200

    Modern Business Bootstrap Theme added

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController extends AbstractController
+{
+    #[Route('/lucky', name: 'app_lucky')]
+    public function index(): Response
+    {
+        return $this->render('lucky/index.html.twig', [
+            'controller_name' => 'LuckyController',
+        ]);
+    }
+    #[Route('/template', name: 'app_template')]
+    public function template(): Response
+    {
+        return $this->render('template.html.twig');
+    }
+}
